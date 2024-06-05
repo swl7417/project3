9dca1282e397a62f6ce2e11435e26b763f950ddf
신원일 <136438606+swl7417@users.noreply.github.com>
2024-06-05 19:50:06 +0900

    Update process2_conv.py

--- manipulator algorithm/process2/process2_conv.py
+++ manipulator algorithm/process2/process2_conv.py
@@ -5,6 +5,7 @@ import serial
 import time
 
 flag = False
+stop = False
 
 def flag_callback(msg):
     global flag
@@ -12,17 +13,33 @@ def flag_callback(msg):
         print("Sending start signal to arduino")
         flag = True
         read_and_publish()  # Start the server request process
+
+def stop_callback(msg):
+    global flag, stop
+    if msg.data == 1 and not flag:
+        print("Sending start signal to arduino")
+        flag = True
+        stop = True
+        read_and_publish()  # Start the server request process
         
 
 def read_and_publish():
-    global flag
+    global flag, stop
     if flag:
         try:
-            ser.write(b'1')
-            print('conv Start!')
-            time.sleep(1)
-            flag = False
+            if stop:
+                ser.write(b'S')
+                print('conv Stop!')
+                time.sleep(1)
+                flag = False
+                stop = False
             
+            else:
+                ser.write(b'1')
+                print('conv Start!')
+                time.sleep(1)
+                flag = False
+                
         except ValueError:
             rospy.logwarn(f"Received invalid data")
 
@@ -34,5 +51,6 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     time.sleep(2)
     rospy.Subscriber("start_conv", Int32, flag_callback)
+    rospy.Subscriber("stop_conv", Int32, stop_callback)
 
     rospy.spin()
